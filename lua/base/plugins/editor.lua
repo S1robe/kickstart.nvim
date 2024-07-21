b5864efbe8315cb98c3abf1805cc56e460820338
Strobe <GarrettPrentice@tuta.io>
2024-07-21 00:38:41 -0700

    Updated themes and keybindings

--- lua/base/plugins/editor.lua
+++ lua/base/plugins/editor.lua
@@ -89,14 +89,14 @@ return {
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'pineapplegiant/spaceduck',
+    'amit-chaudhari1/karma.nvim',
     dependencies = { 'sheerun/vim-polyglot' },
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'spaceduck'
+      vim.cmd.colorscheme 'karma'
     end,
   },
   -- Highlight todo, notes, etc in comments
